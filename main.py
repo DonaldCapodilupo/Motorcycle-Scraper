33223b248ca0a1b401aad3c52e7919636cf03a31
DonaldCapodilupo <Donald@Capodilupo.dev>
2021-05-23 12:33:07 -0400

    Craigslist now crawls. Beginning the process of removing selenium so that the program can be used on a pi 0.

--- main.py
+++ main.py
@@ -69,65 +69,94 @@ def get_Motorcycle_Information_Central_Mass_Powersport(url):
     return information
 
 def get_Motorcycle_Information_Craigslist(url):
-    motorcycle_Information = []
-
-    page_Info = requests.get(url).text
-
-    soup = BeautifulSoup(page_Info, 'html.parser')
     try:
-        title_Row = soup.find('span', class_='postingtitletext')
-        motorcycle_name = title_Row.find('span',  id="titletextonly").text
-        motorcycle_price = title_Row.find('span', class_="price").text
 
-        information_Table = soup.find('div', class_='mapAndAttrs')
-        bike_Information = information_Table.find_all('p', class_='attrgroup')
+        page_Info = requests.get(url).text
 
-        #groups = bike_Information.find_next('p')
+        soup = BeautifulSoup(page_Info, 'html.parser')
+        try:
+            title_Row = soup.find('span', class_='postingtitletext')
+            motorcycle_name = title_Row.find('span',  id="titletextonly").text
+            motorcycle_price = title_Row.find('span', class_="price").text
 
+            information_Table = soup.find('div', class_='mapAndAttrs')
+            bike_Information = information_Table.find_all('p', class_='attrgroup')
 
+            for i in bike_Information:
+                information_Dict = {
+                    'condition': '',
+                    'odometer': '',
+                    'color': '',
+                    'transmission': '',
+                    'engine displacement': '',
+                }
+                for s in i.find_all():
+                    if 'condition' in s.text:
+                        information_Dict['condition'] = s.text[11:]
 
-        for i in bike_Information:
-            information_Dict = {
-                'condition': '',
-                'odometer': '',
-                'color': '',
-                'transmission': '',
-                'engine displacement': '',
-            }
-            for s in i.find_all():
-                if 'condition' in s.text:
-                    information_Dict['condition'] = s.text[11:]
+                    elif 'odometer: ' in s.text:
+                        information_Dict['odometer'] = s.text[9:]
+                    elif 'color: ' in s.text:
+                        information_Dict['color'] = s.text[13:]
+                    elif 'transmission: ' in s.text:
+                        information_Dict['transmission'] = s.text[14:]
+                    elif 'engine displacement: ' in s.text:
+                        information_Dict['engine displacement'] = s.text[19:]
 
-                elif 'odometer: ' in s.text:
-                    information_Dict['odometer'] = s.text[9:]
-                elif 'color: ' in s.text:
-                    information_Dict['color'] = s.text[13:]
-                elif 'transmission: ' in s.text:
-                    information_Dict['transmission'] = s.text[14:]
-                elif 'engine displacement: ' in s.text:
-                    information_Dict['engine displacement'] = s.text[19:]
+                if information_Dict['condition'] != '':
+                    with open('output.csv', 'a', newline='') as csv_File:
 
-            if information_Dict['condition'] != '':
-                with open('output.csv', 'a', newline='') as csv_File:
-
-                    #[today, name, price, stock_Number, color, mileage, transmission]
-                    reader_obj = csv.writer(csv_File)
-                    reader_obj.writerow([today, motorcycle_name, motorcycle_price, "", information_Dict['color'], information_Dict['odometer'],
-                                         information_Dict['transmission'], information_Dict['engine displacement'],information_Dict['condition']])
-    except (AttributeError, TypeError):
+                        #[today, name, price, stock_Number, color, mileage, transmission]
+                        reader_obj = csv.writer(csv_File)
+                        reader_obj.writerow([today, motorcycle_name, motorcycle_price, "", information_Dict['color'], information_Dict['odometer'],
+                                             information_Dict['transmission'], information_Dict['engine displacement'],information_Dict['condition']])
+        except (AttributeError, TypeError):
+            pass
+    except requests.exceptions.MissingSchema:
         pass
 
-links_To_Scrape = ["https://www.newenglandpowersports.com/preowned","https://www.cycledesignonline.com/default.asp?page=xPreOwnedInventory",
-                   "https://boston.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
-                   "https://worcester.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
-                   "https://westernmass.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
-                   "https://capecod.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50"]
-today = str(date.today())
+def craigslistCrawler():
+    for website in links_To_Scrape['Craigslist']:
+
+        reqs = requests.get(website)
+
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+
+        href_tags = soup.find_all(href=True)
+
+        hrefs = [tag.get('href') for tag in href_tags]
+
+        hrefs_No_Duplicates = []
+
+        for link in hrefs:
+            if link not in hrefs_No_Duplicates:
+                hrefs_No_Duplicates.append(link)
 
 
-if __name__ == '__main__':
-    setupFiles()
-    for website in links_To_Scrape:
+
+
+        for link in hrefs_No_Duplicates:
+            try:
+                get_Motorcycle_Information_Craigslist(link)
+                print("Scraped: " + link)
+            except UnicodeEncodeError:
+                if link.title == 'next page':
+                    links_To_Scrape['Craigslist'].append(link)
+                pass
+
+def newEnglandPowersportsCrawler():
+    for website in links_To_Scrape['Central Mass Powersport']:
+
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+
+        href_tags = soup.find_all(href=True)
+        hrefs = [tag.get('href') for tag in href_tags]
+
+
+        for link in hrefs:
+            print(link)
+
+        print(website)
 
         driver = webdriver.Chrome()
         driver.get(website)
@@ -140,40 +169,64 @@ if __name__ == '__main__':
             if href is not None:
                 if href not in motorcycle_Links:
                     motorcycle_Links.append(href)
-
         for link in motorcycle_Links:
-            if "https://www.cycledesignonline.com/" in link:
-                try:
-                    motorcycle_information = get_Motorcycle_Information_Cycle_Design(link)
-                    with open('output.csv', 'a', newline='') as csv_File:
-                        reader_obj = csv.writer(csv_File)
-                        reader_obj.writerow(motorcycle_information)
+            try:
+                motorcycle_information = get_Motorcycle_Information_Central_Mass_Powersport(link)
+                with open('output.csv', 'a', newline='') as csv_File:
+                    reader_obj = csv.writer(csv_File)
+                    reader_obj.writerow(motorcycle_information)
 
-                except (AttributeError, requests.exceptions.InvalidSchema) as error:
-                    #print("Link: " + link + " is not a link to a motorcycle.")
-                    #print(error)
-                    pass
-
-            elif "https://www.newenglandpowersports.com/" in link:
-                if "pdf" in link:
-                    continue
-                try:
-                    motorcycle_information = get_Motorcycle_Information_Central_Mass_Powersport(link)
-                    with open('output.csv', 'a', newline='') as csv_File:
-                        reader_obj = csv.writer(csv_File)
-                        reader_obj.writerow(motorcycle_information)
-
-                except (AttributeError, requests.exceptions.InvalidSchema) as error:
-                    #print("Link: " + link + " is not a link to a motorcycle.")
-                    #print(error)
-                    pass
-            elif "craigslist" in link:
-                try:
-                    get_Motorcycle_Information_Craigslist(link)
-                except UnicodeEncodeError:
-                    if link.title == 'next page':
-                        links_To_Scrape.append(link)
-                    pass
-
-
+            except (AttributeError, requests.exceptions.InvalidSchema) as error:
+                pass
         driver.close()
+
+
+
+
+links_To_Scrape = {'Craigslist':
+                       ["https://boston.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
+                        "https://worcester.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
+                        "https://westernmass.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
+                        "https://capecod.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50",
+                        'https://southcoast.craigslist.org/search/mca?purveyor-input=all&condition=20&condition=30&condition=40&condition=50'
+                        ],
+                   'Cycle Design':["https://www.cycledesignonline.com/default.asp?page=xPreOwnedInventory"],
+                   'Central Mass Powersport':["https://www.newenglandpowersports.com/preowned"]
+}
+today = str(date.today())
+
+
+if __name__ == '__main__':
+    setupFiles()
+
+    craigslistCrawler()
+
+    #newEnglandPowersportsCrawler()
+
+
+
+    #for website in links_To_Scrape['Cycle Design']:
+#
+    #    driver = webdriver.Chrome()
+    #    driver.get(website)
+#
+    #    motorcycle_Links = []
+#
+    #    elems = driver.find_elements_by_tag_name('a')
+    #    for link in elems:
+    #        href = link.get_attribute('href')
+    #        if href is not None:
+    #            if href not in motorcycle_Links:
+    #                motorcycle_Links.append(href)
+    #    for link in motorcycle_Links:
+    #        try:
+    #            motorcycle_information = get_Motorcycle_Information_Cycle_Design(link)
+    #            with open('output.csv', 'a', newline='') as csv_File:
+    #                reader_obj = csv.writer(csv_File)
+    #                reader_obj.writerow(motorcycle_information)
+#
+    #        except (AttributeError, requests.exceptions.InvalidSchema) as error:
+    #            pass
+    #    driver.close()
+
+
